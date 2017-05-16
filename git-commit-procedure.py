a700f546ddf47d52db4a6b2f40dec797edab2c4f
fwrjr398 <fredrick.were@capitalone.com>
2017-05-16 00:09:41 -0400

    second python-history

---
+++ git-commit-procedure.py
@@ -0,0 +1,15 @@
+useradd fwrjr398
+   passwd fwrjr398
+  git config --global user.name "fwrjr398"
+  git config --global user.email "fredrick.were@capitalone.com"
+  passwd fwrjr398
+  git clone https://github.com/fwrjr398/mytoc-repo17.git
+  cd mytoc-repo17/
+  vim tkinter.py
+  chmod 755 tkinter.py
+  
+  git add tkinter.py
+  git status
+  git commit -m "first python commit" tkinter.py
+  git push -u origin master
+
